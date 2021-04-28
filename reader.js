63d31f7dca3d76df9e5ad92065ce0b887960c00d
lorenmahia <78753917+lorenmahia@users.noreply.github.com>
2021-04-27 22:42:11 -0400

    Update reader.js

--- reader.js
+++ reader.js
@@ -45,4 +45,4 @@ http.createServer(function (req, res)   {
                  });})
         });  } 
      else   {  res.writeHead(200, {'Content-Type':'text/html'}); 
-             res.write ("Unknown");  res.end();  }}).listen(8080);
+             res.write ("Unknown");  res.end();  }}).listen(process.env.PORT || 3000);
